af993931c16c9dab35668e2607916eb8ffc621aa
freddy208 <kfreddypatient@gmail.com>
2025-11-15 03:25:38 +0100

    Initialisation projet

---
+++ config/cors.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'paths' => ['api/*'],
+    'allowed_methods' => ['*'],
+    'allowed_origins' => ['*'], // En production, remplace par ton domaine Vercel
+    'allowed_origins_patterns' => [],
+    'allowed_headers' => ['*'],
+    'exposed_headers' => [],
+    'max_age' => 0,
+    'supports_credentials' => false,
+];
